fad6d1e6992f7a1fc82685be1e2cb50070444447
Thaily <lyvinhthai321@gmail.com>
2024-07-11 22:56:02 +0700

    Web_music 0.02

--- src/app/components/Banner/BannerHome.tsx
+++ src/app/components/Banner/BannerHome.tsx
@@ -7,7 +7,7 @@ export default function BannerHome() {
           <div className="font-[500] text-[14px] text-white">Top 100 Nhạc Electronic/Dance Âu Mỹ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Top 100 Nhạc Electronic/Dance Âu Mỹ</div>
         </div>
         <div className="w-[215px] mr-[23px] mt-[40px]">
-          <img src="/image/img-1.svg" alt="img-1" />
+          <img src="/image/Img-1.svg" alt="img-1" />
         </div>
       </div>
     </>
